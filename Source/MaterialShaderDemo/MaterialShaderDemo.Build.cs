8092c1e1ac3bd8f4be5bc2a21f5e6522964b2474
Phyronnaz <phyronnaz@gmail.com>
2025-06-16 22:55:05 +0200

    First commit

---
+++ Source/MaterialShaderDemo/MaterialShaderDemo.Build.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class MaterialShaderDemo : ModuleRules
+{
+	public MaterialShaderDemo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
